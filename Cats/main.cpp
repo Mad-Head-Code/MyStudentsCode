5d559bc7ab01c5eab2b3945785b7487bb152c8af
Mad-Head <nikolaizimin1995@gmail.com>
2023-11-23 11:00:37 +0300

    Cats:|

---
+++ Cats/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    /*cout << "( *-*)/\(0o0?)" << endl;
+    return 0;
+    int a=atoi(argv[1]);
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  0 o 0|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/    \\|"<<endl;*/
+
+    int a=atoi(argv[1]);
+    while(true){
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  0 o 0|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/     \\/"<<endl;
+    Sleep(2);
+    system("cls");
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  o o o|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/     \\/"<<endl;
+    Sleep(2);
+    system("cls");
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  - o -|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/     \\/"<<endl;
+    Sleep(2);
+    system("cls");
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  - o -|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/     \\/"<<endl;
+    Sleep(2);
+    system("cls");
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  - o -|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/     \\/"<<endl;
+    Sleep(2);
+    system("cls");
+    cout<<"/\\-----/\\"<<endl;
+    cout<<"|  o o o|   miaw!!"<<endl;
+for(int i=0;i<a;i++){
+    cout<<"|       |"<<endl;
+}
+    cout<<"|  ---- |"<<endl;
+    cout<<"\\/     \\/"<<endl;
+    Sleep(2);
+    system("cls");
+    }
+}
